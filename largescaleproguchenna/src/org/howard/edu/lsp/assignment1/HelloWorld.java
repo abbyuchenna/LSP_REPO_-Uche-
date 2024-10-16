c6c86f56378a9b578ac309dd2035df4ccf33a582
abigailaboah <131496831+abigailaboah@users.noreply.github.com>
2024-10-16 19:15:24 -0400

    first commit

---
+++ largescaleproguchenna/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+* Name: Abigail Aboah
+*/
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+		// TODO Auto-generated method stub
+
+	}
+
+}
